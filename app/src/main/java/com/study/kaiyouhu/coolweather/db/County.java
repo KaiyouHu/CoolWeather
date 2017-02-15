73fc2d9f63d4deaa4b602669105733f659764079
kaiyouhu <kaiyouhu@gmail.com>
2017-02-15 22:25:26 +0800

    add database

---
+++ app/src/main/java/com/study/kaiyouhu/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.study.kaiyouhu.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ASUS on 2017/2/15.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
---
+++ app/src/main/java/com/study/kaiyouhu/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.study.kaiyouhu.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ASUS on 2017/2/15.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
