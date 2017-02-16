4b2304ee6a5408117bd86543e4b81a92cd243377
kaiyouhu <kaiyouhu@gmail.com>
2017-02-16 16:46:51 +0800

    Afternoon Sixteen Feb

--- app/src/main/java/com/study/kaiyouhu/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/study/kaiyouhu/coolweather/util/HttpUtil.java
@@ -1,8 +1,16 @@
 package com.study.kaiyouhu.coolweather.util;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by ASUS on 2017/2/16.
  */
 
 public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
 }
--- app/src/main/java/com/study/kaiyouhu/coolweather/util/Utility.java
+++ app/src/main/java/com/study/kaiyouhu/coolweather/util/Utility.java
@@ -1,8 +1,80 @@
 package com.study.kaiyouhu.coolweather.util;
 
+import android.text.TextUtils;
+
+
+import com.study.kaiyouhu.coolweather.db.City;
+import com.study.kaiyouhu.coolweather.db.County;
+import com.study.kaiyouhu.coolweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by ASUS on 2017/2/16.
  */
 
 public class Utility {
+    //解析和处理服务器返回的数据
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i=0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    //解析和处理服务器返回的数据
+    public static boolean handleCityResponse(String response,int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i=0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(provinceObject.getString("name"));
+                    city.setCityCode(provinceObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    //解析和处理服务器返回的数据
+    public static boolean handleCountyResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCounty = new JSONArray(response);
+                for(int i=0;i<allCounty.length();i++){
+                    JSONObject countyObject = allCounty.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 }
