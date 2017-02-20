62218fc87ea0333f3d5874834ed92d3862b17272
kaiyouhu <kaiyouhu@gmail.com>
2017-02-20 17:40:55 +0800

    完成后台更新，更换图标

--- app/src/main/java/com/study/kaiyouhu/coolweather/WeatherActivity.java
+++ app/src/main/java/com/study/kaiyouhu/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.study.kaiyouhu.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.study.kaiyouhu.coolweather.gson.Forecast;
 import com.study.kaiyouhu.coolweather.gson.Weather;
+import com.study.kaiyouhu.coolweather.service.AutoUpdateService;
 import com.study.kaiyouhu.coolweather.util.HttpUtil;
 import com.study.kaiyouhu.coolweather.util.LogUtil;
 import com.study.kaiyouhu.coolweather.util.Utility;
@@ -217,6 +219,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
 
     }
 
